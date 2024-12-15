a3f5e73230f779f25742eab116d544402daaee8d
JanhviVatsya <janhvivatsya24@gmail.com>
2024-12-15 10:40:31 +0530

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Home
